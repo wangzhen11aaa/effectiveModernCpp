0ea42e50e83b78c4c84b50b1498baaddb73e201d
wangzhen05 <wangzhen05@meicai.cn>
2020-05-06 14:37:59 +0800

    override test on multiple-level inheritance model, and add the & and && qualifier

--- item12_declare_override.cpp
+++ item12_declare_override.cpp
@@ -20,6 +20,25 @@ private:
     dataType data;
 };
 
+class Base{
+    public:
+        virtual void mf1() const & { cout << "Invoke Base's mf1() function" << endl;}
+        virtual void mf1() const && { cout << "Rvalue Invoke Base's mf1() function" << endl;}
+
+};
+// better with the override keyword.
+class Derived0 : public Base{
+    public:
+        virtual void mf1() const & override {cout << "Invoke Derived0's mf1() function" << endl;}
+        virtual void mf1() const && override { cout << "Rvalue Invoke Base's mf1() function" << endl;}
+};
+
+class Derived1: public Derived0{
+    public:
+        virtual void mf1()  const & override {cout << "Invoke Derived1's mf1() function" << endl;}
+        virtual void mf1() const && override { cout << "Rvalue Invoke Base's mf1() function" << endl;}
+};
+
 Widget makeWidget(){
     dataType d{1,2};
     Widget w(d);
@@ -35,5 +54,20 @@ int main(){
     }
     // use factory;
     auto _t = makeWidget().get();
+
+    // Test the grandpa->father-son derivation.
+    Derived1 d1;
+    Base & b1 = d1;
+    cout << "Invoke mf1 from Base reference to d1" << endl;
+    b1.mf1();
+    Derived0 &d0 = d1;
+    cout << "Invoke mf1 from Derived0 reference to d1" << endl;
+    d0.mf1();
+
+    // I did not figure out why this rvalue can invoke
+    // lvalue function type.
+    Derived1().mf1();
+    Derived0().mf1();
+    Base().mf1();
     return 0;
 }
