a38031fbb1206f76b1d206679d72f7220169ddd2
wangzhen05 <wangzhen05@meicai.cn>
2020-05-11 09:31:56 +0800

    add new information about closure (lambda) thing

--- item31_avoid_default_capture.cpp
+++ item31_avoid_default_capture.cpp
@@ -5,7 +5,86 @@
 #include "type_name.hpp"
 
 using namespace std;
+/*
+    A lambda expression is just that: an expression. It's part of source code. In
+    std::find_if(container.begin(), container.end(), [](int val){return 0 < val && val < 10;})
 
+    A clousure is the runtime object created by a lamdba.Depending on the capture mode, closures
+    hlod copies of or references to the captured data.In the call to std::find_if above, the 
+    closure is the object that's passed at runtime as the third argument to std::find_if.
+
+    A closure class is a class from which a closure is instantiated.Each lambda causes compilers to
+    generate a unique closure class. The statements inside a lambda become executable instructions
+    in the member functions of its closure class.
+
+    A lambda is often used to create a clousure that's used only as an argument to a function.
+    That's the case in the call to std::find_if above. However, closures may generally be copieed, 
+    so it's usually possible to have multiple closures of a closure type corresponding to a single 
+    lambda. For example, in the following code,
+
+    {
+        int x; // x is local variable.
+        ...
+        auto c1 = [x](int y){return x * y > 55;} // c1 is copy of the closure produced by lambda
+
+        auto c2 = c1; // c2 is copy of c1
+        auto c3 = c2; // c3 is copy of c2
+
+
+    }
+    c1,c2,and c3 are all copies of the closure produced by the lambda.
+
+    Informally, it's perfectly acceptable to blur the lines between lambdas, closures, and 
+    closure classes.But in the Items that follow, it's often important to distinguish what 
+    exists during compilation(lambdas and closure classes), what exits at runtime(closures),
+    and how they relate to one another.
+*/
+
+/*
+    A by-reference capture causes a closure to contain a reference to a local variable or 
+    to a parameter that's available in the scope where lambda is defined. If the lifetime
+    of a closure created from that lambda exceeds the lifetime of the local variable or 
+    parameter, the reference in the closure will dangle.For example, suppose we have a container
+    of filtering functions, each of which takes an int and return bool indicating whether a 
+    passed-in value satisfies the filter:
+
+    {
+        using FilterContainer = std::vector<std::function<bool(int)>>;
+
+        FilterContainer filters; //  filtering funcs
+
+        void addDivisorFilter()
+        {
+            auto calc1 = computeSomeValue1(); 
+            auto calc2 = computeSomeValue2(); //
+            auto divisor = computeDivisor(calc1, calc2);
+            filters.emplace_back([&](int value){return value % divisor == 0;})
+        }
+        This code is a problem waiting to happen. The lambda refers to the local variable
+        divisor, but that variable ceases to exist when addDivisorFilter returns. That's 
+        immediately after filters'emplace_back returuns, so the function that' added to filters
+        is essentially dead on arrival. Using that filter yields undefined behavior from
+        from virtually the moment it's created.
+
+        Now, the same problem would exist if divisor's by-reference capture were explict,
+        filters.emplace_back(
+            [&divisor](int value){return value % divisor == 0;} // danger! ref to divisor will still dangle!
+        )
+        but with an explicit capture, it's easier to see that the viability of the lambda is depen-
+        dent on divisor's lifetime. Also, writing out the name, "divisor", reminds us to ensure
+        that divisor lives at least as long as the lambda's closures. That's more specific memory
+        jog than general "make sure nothing dangles" admonition that "[&]" conveys.
+
+        If you know that a closure will be used immediately(e.g, by being passed to an STL 
+        algorithm) and won't be copied, there is no risk that references it holds will outlive
+        the local variables and parameters in the environment where its lambda is created. In 
+        that case, you might argue, there's no risk of dangling references, hence no reason to 
+        avoid a default by-reference capture mode.For example, our filtering lambda might be 
+        used only as an argument of C++11 std::all_of, which returns whether all elements in
+        range satisfy a condition:
+        
+    }
+*/
 class Widget{
 public:
     Widget(int v=0):divisor(v){};
